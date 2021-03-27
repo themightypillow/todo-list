94ba6d39d86724efbc4b8538348f67b7878e2dd8
Cheri <mightypillow@tutanota.com>
2021-03-27 18:58:17 -0400

    Add task form displays on click

--- src/UI.js
+++ src/UI.js
@@ -23,9 +23,11 @@ const UI = (() => {
     const addTaskText = document.createElement("div");
     addTaskText.textContent = "Add Task";
     addTask.appendChild(addTaskText);
+    addTask.addEventListener("click", e => {
+      const form = document.querySelector("#task-form")
+      form.style.display = "block";
+    });
     main.appendChild(addTask);
-
-    // if(e.currentTarget.id) console.log(todo.getById(e.currentTarget.id).getName());
   };
 
   const addToSidebar = (name, id) => {
@@ -42,10 +44,11 @@ const UI = (() => {
     return newProject;
   }
 
-  // load any existing projects from storage
+  // load existing projects from storage and display first
   (function() {
     todo.load();
     Object.entries(todo.getProjects()).forEach(pair => addToSidebar(pair[1], pair[0]));
+    displayProject(document.querySelector("#projects > div"));
   })();
 
   // display project groups on click
@@ -53,6 +56,15 @@ const UI = (() => {
     const groups = document.querySelectorAll(".group");
     groups.forEach(group => {
       group.addEventListener("click", e => displayProject(e.currentTarget));
+    });
+  })();
+
+  // disable enter key behavior in text inputs
+  (function() {
+    document.querySelectorAll("form > input[type='text']").forEach(input => {
+      input.addEventListener("keypress", e => {
+        if(e.key === "Enter") e.preventDefault();
+      });
     });
   })();
 
@@ -66,18 +78,13 @@ const UI = (() => {
 
     const nameLabel = document.querySelector("#new-project-name");
     const cancel = document.querySelector("#cancel-project");
-    const submit = document.querySelector("#submit-project");
-
-    nameLabel.addEventListener("keypress", e => {
-      if(e.keyCode === 13) e.preventDefault();
-    });
-
     cancel.addEventListener("click", e => {
       e.preventDefault();
       nameLabel.value = "";
       form.style.display = "none";
     });
 
+    const submit = document.querySelector("#submit-project");
     submit.addEventListener("click", e => {
       e.preventDefault();
       form.style.display = "none";
@@ -88,4 +95,32 @@ const UI = (() => {
     });
   })();
 
+  // set up adding new task
+  (function() {
+    const form = document.querySelector("#task-form");
+    const title = document.querySelector("#new-task-title");
+    const desc = document.querySelector("#new-task-desc");
+    const due = document.querySelector("#new-task-due");
+    const prio = document.querySelector("#new-task-prio");
+
+    const clearForm = () => {
+      title.value = "";
+      desc.value = "";
+      due.value = "";
+      prio.checked = false;
+      form.style.display = "none";
+    };
+
+    const cancel = document.querySelector("#cancel-task");
+    cancel.addEventListener("click", e => {
+      e.preventDefault();
+      clearForm();
+    });
+
+    const add = document.querySelector("#submit-task");
+    add.addEventListener("click", e => {
+      e.preventDefault();
+      clearForm();
+    });
+  })();
 })();
